ffd026a5103508d6bac12d190ee6e1809f597ba2
root <root@zkvm-1-testnet-0.c.parity-lab.internal>
2025-07-14 09:56:48 +0000

    new input blobs for SHA1

--- run-pvm-sha1.sh
+++ run-pvm-sha1.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 
 echo "Running benchmark (PVM): SHA1"
 #cargo run --release -p pvm-host blobs/sha1-demo.polkavm blobs/guest-program.bin
-cargo run --release -p pvm-host blobs/sha1-demo.polkavm blobs/sha1.input
+cargo run --release -p pvm-host blobs/sha1-demo.polkavm blobs/sha1-1k.input
+cargo run --release -p pvm-host blobs/sha1-demo.polkavm blobs/sha1-10k.input
+cargo run --release -p pvm-host blobs/sha1-demo.polkavm blobs/sha1-100k.input
+cargo run --release -p pvm-host blobs/sha1-demo.polkavm blobs/sha1-1m.input
+cargo run --release -p pvm-host blobs/sha1-demo.polkavm blobs/sha1-10m.input
