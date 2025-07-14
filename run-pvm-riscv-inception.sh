006eb20fc31a9bb6e2c74391d13155cd385072e2
root <root@zkvm-5-testnet-0.c.parity-lab.internal>
2025-07-14 16:51:01 +0000

    scripts for native and pvm execution

--- run-pvm-riscv-inception.sh
+++ run-pvm-riscv-inception.sh
@@ -2,5 +2,18 @@
 
 set -euo pipefail
 
+counts=("10" "100" "1k" "10k" "100k" "1m" "10m" "100m" "full")
+home=$(pwd)
+
 echo "Running benchmark (PVM): RISC-V inception"
-cargo run --release -p pvm-host blobs/riscv-inception.polkavm blobs/guest-program.bin
+for c in "${counts[@]}"; do
+	cd benchmarks/riscv-inception/rust/src
+	outfile="${home}/logs/riscv-pvm-${c}.log"
+	patch="../../../../patches/patch-${c}.patch"
+	patch -p0 < $patch
+	cd ../
+	./build.sh
+	cd $home
+	cargo run --release -p pvm-host blobs/riscv-inception.polkavm blobs/guest-program.bin > "$outfile"
+	git stash push -- benchmarks/
+done
---
+++ run-riscv-native.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+counts=("10" "100" "1k" "10k" "100k" "1m" "10m" "100m" "full")
+home=$(pwd)
+
+echo "Running benchmark (Native): RISC-V"
+cd benchmarks/riscv-inception/rust/src;
+for c in "${counts[@]}"; do
+	patch="../../../../patches/patch-${c}.patch"
+	outfile="${home}/logs/riscv-native-${c}.log"
+	patch -p0 < $patch
+	cargo run --release > "$outfile"
+	git stash push -- .
+done
+
